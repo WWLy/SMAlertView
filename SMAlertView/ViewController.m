9d072f7cc0c5945bdf8a1d3bbf7abd37fac6b778
刘彪 <liubiao@smart360.cn>
2017-02-15 18:10:35 +0800

    完善DEMO样例

--- SMAlertView/ViewController.m
+++ SMAlertView/ViewController.m
@@ -15,17 +15,18 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    // 创建一个显示在window上的alert
     SMAlertView *alert = [[SMAlertView alloc] initWithNewWindow];
     
+    // 横向排列按钮
     [alert setHorizontalButtons:YES];
     
-    SMAlertViewButton *button = [alert addButton:@"Firsrt Button" target:self selector:@selector(firstButton)];
+    // 通过SEL方式添加按钮
+    SMAlertViewButton *button = [alert addButton:@"Firsrt Button" target:self selector:@selector(firstButtonClick)];
+    
+    // 设置按钮的样式
     button.buttonFormatBlock = ^NSDictionary* (void)
     {
         NSMutableDictionary *buttonConfig = [[NSMutableDictionary alloc] init];
@@ -38,34 +39,50 @@
         return buttonConfig;
     };
     
+    // 通过block方式添加按钮
     [alert addButton:@"Second Button" actionBlock:^(void) {
         NSLog(@"Second button tapped");
     }];
     
+    // 设置完成按钮的样式
+    alert.completeButtonFormatBlock = ^NSDictionary* (void)
+    {
+        NSMutableDictionary *buttonConfig = [[NSMutableDictionary alloc] init];
+        
+        buttonConfig[@"backgroundColor"] = [UIColor clearColor];
+        buttonConfig[@"textColor"] = [UIColor blueColor];
+        buttonConfig[@"borderWidth"] = @0.0f;
+        buttonConfig[@"borderColor"] = [UIColor clearColor];
+        
+        return buttonConfig;
+    };
     
+    // 点击蒙版消失
+    alert.shouldDismissOnTapOutside = YES; // Default: NO
+    
+    // 开始移除时触发
     [alert alertIsDismissed:^{
         NSLog(@"--");
     }];
     
-    // 动画结束后触发
+    // 移除动画结束后触发
     [alert alertDismissAnimationIsCompleted:^{
         NSLog(@"----");
     }];
     
+    // 显示动画类型
     alert.showAnimationType = SMAlertViewShowAnimationSlideInToCenter;
+    // 移除动画类型
     alert.hideAnimationType = SMAlertViewHideAnimationFadeOut;
     
+    // 关键方法, 显示alert
     [alert showTitle:nil color:[UIColor greenColor] title:nil subTitle:@"请确认输入项是否正确, 请检查网络连接" completeText:@"complete"];
 }
 
-- (void)firstButton {
+- (void)firstButtonClick {
     NSLog(@"First button tapped");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
